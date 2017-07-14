269832ec5f1ccea850d2bdc1c0af6826e66bddf0
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-07-14 10:33:28 -0700

    add routerlink to each member's name that will link to the member compoenent

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MemberListComponent } from './member-list/member-list.component';
 import { AboutComponent } from './about/about.component';
+import { MemberProfileComponent } from './member-profile/member-profile.component';
 
 const appRoutes: Routes = [
   {
@@ -11,6 +12,10 @@ const appRoutes: Routes = [
   {
     path: "about",
     component: AboutComponent
+  },
+  {
+    path: "members/:id",
+    component: MemberProfileComponent
   }
 ];
 
