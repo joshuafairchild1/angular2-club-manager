31942e58ec4b0d98ae77301ba7cdb6ab689ca155
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-07-14 10:30:02 -0700

    add navbar to root component, containing links to the root path and to the about component, which now has some content

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MemberListComponent } from './member-list/member-list.component';
+import { AboutComponent } from './about/about.component';
 
 const appRoutes: Routes = [
   {
     path: "",
     component: MemberListComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
   }
 ];
 
